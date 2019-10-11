4431b8899037d8b90de1585954d3601ccbd88381
nventer <nventer@luno.com>
2019-10-11 12:26:28 +0200

    added rpc methods

--- api.go
+++ api.go
@@ -1,5 +1,11 @@
 package unsure
 
-type UhtClient interface {
+import (
+	"context"
+	"github.com/luno/reflex"
+)
 
+type UhtClient interface {
+	Ping(context.Context) error
+	Stream(ctx context.Context, after string, opts ...reflex.StreamOption) (reflex.StreamClient, error)
 }
