b51646e39ff14c0bd391eb6abd7ccd1e817df07b
Marius de Wit <mdewit@luno.com>
2019-10-11 10:17:11 +0200

    Add events and cursors tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,16 @@ create table `match`
   status int
 );
 
+create table `match_events`
+(
+    id bigint not null auto_increment,
+    foreign_id bigint not null,
+    timestamp datetime not null,
+    type int not null,
+
+    primary key (id)
+);
+
 create table `round`
 (
   id bigint primary key,
@@ -11,3 +21,22 @@ create table `round`
   match_id bigint, -- foreign key
   data text
 );
+
+create table `round_events`
+(
+  id bigint not null auto_increment,
+  foreign_id bigint not null,
+  timestamp datetime not null,
+  type int not null,
+
+  primary key (id)
+);
+
+create table `uht_cursors`
+(
+    id varchar(255) not null,
+    last_event_id bigint not null default 0,
+    updated_at datetime not null,
+
+    primary key (id)
+)
